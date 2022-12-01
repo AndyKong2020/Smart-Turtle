71857af4507e09cca37ef67572a6a7e4ff0c2bbf
AndyKong <andy.kong@nuaa.edu.cn>
2022-12-02 00:41:06 +0800

    [feat]lidar driver done

---
+++ ydlidar_ws/build/ydlidar_ros_driver/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/andynuc/ydlidar_ws/src/ydlidar_ros_driver
+# Build directory: /home/andynuc/ydlidar_ws/build/ydlidar_ros_driver
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
